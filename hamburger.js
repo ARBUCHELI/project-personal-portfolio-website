e25e44913e48313880a927726ffdcf8eb02c9fc8
ANDRÉS R. BUCHELI (Andrés Fernando Regalado Bucheli) <radio_seattle@hotmail.com>
2026-02-18 02:25:16 +0300

    update

---
+++ hamburger.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "topnav";
+  }
+}
